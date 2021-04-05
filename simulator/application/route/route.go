26a2ae2b8159fe733c93effd0b8da17ffeb0d600
xXHachimanXx <globaldep90@gmail.com>
2021-04-05 17:24:37 -0300

    Reading files with positions.

--- simulator/application/route/route.go
+++ simulator/application/route/route.go
@@ -10,3 +10,19 @@ type Position struct {
 	Lat float64
 	Long float64
 }
+
+func (r *Route) LoadPositions() error {
+	if r.ID == "" {
+		return errors.New("Route ID not informed!")
+	}
+
+	f, err := os.Open("detinations/" + r.ID + ".txt")
+
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	
+}
