351fbf13f6dc01992a6d4518453a5be06425d139
xXHachimanXx <globaldep90@gmail.com>
2021-04-07 13:45:25 -0300

    Adding kafka test settings.

--- nest-api-v1/src/app.module.ts
+++ nest-api-v1/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoutesModule } from './routes/routes.module';
